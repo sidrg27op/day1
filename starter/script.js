7ca09783c53810389c68d4348e9e7ea8d4825055
siddharth raghav <thakurraghav27@gmail.com>
2022-07-11 11:37:02 +0530

    first challange

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,21 @@ let s = 232;
 let d = 222;
 
 console.log(s <= d);
+
+const johnWeight = 85;
+const johnHeight = 1.76;
+
+const johnBmi = johnWeight / johnHeight ** 2;
+
+console.log(johnBmi);
+
+const MarkWeight = 95;
+const MarkHeight = 1.88;
+
+const MarkBmi = MarkWeight / MarkHeight ** 2;
+
+console.log(MarkBmi);
+
+const Markhigherbmi = MarkBmi > johnBmi;
+
+console.log(Markhigherbmi);
